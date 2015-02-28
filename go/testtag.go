e91c31bbc25cf71e11ad574fa52ede3eecb25c45
ximing <zhaoxmustc@gmail.com>
2015-02-28 14:02:48 +0800

    add test reflect of go

---
+++ go/testtag.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"reflect"
+	"strconv"
+)
+
+type S struct {
+	Name string `paramName:"username"`
+	Age  int    `paramName:"userage"`
+}
+
+func structToMap(i interface{}) (values url.Values) {
+	values = url.Values{}
+	iVal := reflect.ValueOf(i).Elem()
+	typ := iVal.Type()
+	for i := 0; i < iVal.NumField(); i++ {
+		f := iVal.Field(i)
+		// You ca use tags here...
+		tag := typ.Field(i).Tag.Get("paramName")
+		// Convert each type into a string for the url.Values string map
+		var v string
+		switch f.Interface().(type) {
+		case int, int8, int16, int32, int64:
+			v = strconv.FormatInt(f.Int(), 10)
+		case uint, uint8, uint16, uint32, uint64:
+			v = strconv.FormatUint(f.Uint(), 10)
+		case float32:
+			v = strconv.FormatFloat(f.Float(), 'f', 4, 32)
+		case float64:
+			v = strconv.FormatFloat(f.Float(), 'f', 4, 64)
+		case []byte:
+			v = string(f.Bytes())
+		case string:
+			v = f.String()
+		}
+		values.Set(tag, v)
+	}
+	return
+}
+func main() {
+	s := S{
+		Name: "Bob",
+		Age:  15,
+	}
+	urlvaules := structToMap(&s)
+	fmt.Println(urlvaules.Encode())
+}
